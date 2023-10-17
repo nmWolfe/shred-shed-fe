3f50fdfa942532f30898b6bc30d774a79857573d
Nick <n.m.phelan@gmail.com>
2023-10-17 17:13:19 +0100

    added nav component wireframe

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import "./Nav.scss"
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            
+        </div>
+    )
+}
+
+export default Nav;
